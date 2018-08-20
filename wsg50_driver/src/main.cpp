3570fc582ffefef419e6c0dd37048d7953277bee
Zaki <rfa-mr@ipa.fhg.de>
2018-08-20 14:38:34 +0200

    make fault acknowledgment not allowed during motion

--- wsg50_driver/src/main.cpp
+++ wsg50_driver/src/main.cpp
@@ -458,6 +458,10 @@ bool setForceSrv(wsg50_common::Conf::Request &req, wsg50_common::Conf::Response 
 
 bool ackSrv(std_srvs::Empty::Request &req, std_srvs::Empty::Request &res)
 {
+    if (in_motion){
+        ROS_WARN("Acknowledge fault is not allowed while gripper is in motion.");
+        return false;
+    }
     ack_fault();
     return true;
 }
